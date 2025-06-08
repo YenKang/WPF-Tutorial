59025955ec0b65dc32f1c75d2996086fe33510aa
unknown <rcd1988715@gmail.com>
2025-06-08 22:28:19 +0800

    add folder HelloWPF for Data Binding Leaning

---
+++ HelloWPF/HelloWPF/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace HelloWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private string boundText;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set
+            {
+                boundText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+                //OnPropertyChanged(nameof(BoundText));
+            }
+        }
+    }
+}
